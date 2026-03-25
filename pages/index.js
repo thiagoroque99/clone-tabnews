9fcfb95d004886db25913c44cf446559e9acfd35
ThiagoRoq <thiago.roque@sasi.com.br>
2026-03-25 03:05:06 -0400

    salvando um novo parágrado

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,23 @@ function Paragrafo(){
     return <p> O tigre é o maior felino e na caça ele é implacável</p>
 }
 
+function Paragrafo2(props){
+    console.log(props)
+    return <p> O leão é o rei da selva e é conhecido por sua juba majestosa</p>
+}
+
+function Paragrafo3(){
+    return <p> O guepardo é o animal terrestre mais rápido, capaz de atingir velocidades impressionantes</p>
+}
+
 function Home(){
     return (
         <div>
             <Title />
             <Titulo />
             <Paragrafo />
+            <Paragrafo2 />
+            <Paragrafo3 />
         </div>
     )
 }
